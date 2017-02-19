25608fa533c1fd9ed1b9172841e2e6ea1efe937f
Andrew Edwards <andrewthomasedwards@gmail.com>
2017-02-18 20:35:21 -0500

    turn off autohelp and restrict the command to botcontrol

--- plugins/check_conn.py
+++ plugins/check_conn.py
@@ -1,7 +1,7 @@
 from cloudbot import hook
 
 
-@hook.command
+@hook.command(autohelp=False, permissions=["botcontrol"])
 def conncheck(nick, bot, notice): 
     """This command is an effort to make the bot reconnect to a network if it has been disconnected."""
     # For each irc network return a notice on the connection state and send a message from
