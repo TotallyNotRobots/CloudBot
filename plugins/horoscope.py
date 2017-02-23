49a9c39e8872fb06992ca15ddcbf41a19784e235
Andrew Edwards <andrewthomasedwards@gmail.com>
2017-02-22 21:34:47 -0500

    a bit of additional clean up for horoscope.py

--- plugins/horoscope.py
+++ plugins/horoscope.py
@@ -18,7 +18,21 @@ def init(db):
 @hook.command(autohelp=False)
 def horoscope(text, db, bot, notice, nick):
     """<sign> - get your horoscope"""
-    signs = {'aries': '1', 'taurus': '2', 'gemini': '3', 'cancer': '4', 'leo': '5', 'virgo': '6', 'libra': '7', 'scorpio': '8', 'sagittarious': '9', 'capricorn': '10', 'aquarius': '11', 'pisces': '12'}
+    signs = {
+            'aries': '1',
+            'taurus': '2',
+            'gemini': '3',
+            'cancer': '4',
+            'leo': '5',
+            'virgo': '6',
+            'libra': '7',
+            'scorpio': '8',
+            'sagittarious': '9',
+            'capricorn': '10',
+            'aquarius': '11',
+            'pisces': '12'
+    }
+
     headers = {'User-Agent': bot.user_agent}
 
     # check if the user asked us not to save his details
@@ -26,7 +40,7 @@ def horoscope(text, db, bot, notice, nick):
     if dontsave:
         sign = text[:-9].strip().lower()
     else:
-        sign = text
+        sign = text.strip().lower()
 
     db.execute("create table if not exists horoscope(nick primary key, sign)")
 
@@ -36,12 +50,16 @@ def horoscope(text, db, bot, notice, nick):
         if not sign:
             notice("horoscope <sign> -- Get your horoscope")
             return
-        sign = sign[0]
+        sign = sign[0].strip().lower()
 
-    url = "http://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign={}".format(signs[sign.strip().lower()])
+    params = {
+            "sign": signs[sign]
+    }
+
+    url = "http://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx"
 
     try:
-        request = requests.get(url, headers=headers)
+        request = requests.get(url, params=params, headers=headers)
         request.raise_for_status()
     except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
         return "Could not get horoscope: {}. URL Error".format(e)
