13732e93304abef97069baecf1a4bfcd1764012f
Erik Dillingham <erik.dillingham@gmail.com>
2017-03-29 18:52:07 -0700

    added "brew" plugin and config file entry

---
+++ plugins/brew.py
@@ -0,0 +1,57 @@
+import requests
+
+from cloudbot import hook
+
+api_url = "http://api.brewerydb.com/v2/search?format=json"
+
+def api_get(query):
+    """Use the RESTful Google Search API"""
+    url = 'http://api.brewerydb.com/v2/search?q=%s' \
+          '&type=beer'
+    return http.get_json(url % query)
+
+@hook.command('brew')
+def brew(text, bot):
+    """<query> - returns the first brewerydb search result for <query>"""
+
+    api_key = bot.config.get("api_keys", {}).get("brewerydb")
+    if not api_key:
+        return "No brewerydb API key set."
+
+    params = {'key': api_key, 'type': 'beer', 'withBreweries': 'Y', 'q': text}
+    request = requests.get(api_url, params=params)
+
+    if request.status_code != requests.codes.ok:
+        return "Failed to fetch info ({})".format(request.status_code)
+
+    response = request.json()
+    print(response)
+
+    output = "No results found."
+
+    if response['totalResults'] > 0:
+    	beer = response['data'][0]
+    	brewery = beer['breweries'][0]
+    	
+    	name = beer['nameDisplay']
+    	style = beer['style']['shortName']
+    	abv = beer['abv']
+    	
+    	brewer = brewery['name']
+    	brewer_url = brewery['website']
+
+    	output = "{} by {} ({}, {}% ABV) - {}".format(name, brewer, style, abv, brewer_url)
+
+    return output
+#    pass
+#
+#    if not 200 <= parsed['responseStatus'] < 300:
+#        raise IOError('error searching for pages: {}: {}'.format(parsed['responseStatus'], ''))
+#
+#    if not parsed['responseData']['results']:
+#        return 'No results found.'
+#
+#
+#    url = web.try_shorten('http://please.dont.fart')
+#    
+#    return url
