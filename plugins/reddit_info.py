d5dcd74493b907b68dd0c2f5691ec206ec34b4a6
linuxdaemon <linuxdaemon@snoonet.org>
2017-09-10 14:18:34 -0500

    Switch reddit_info.py to using generic pager

--- plugins/reddit_info.py
+++ plugins/reddit_info.py
@@ -1,36 +1,20 @@
-import requests
-
 from collections import defaultdict
 from datetime import datetime
+
+import requests
 from bs4 import BeautifulSoup
+
 from cloudbot import hook
 from cloudbot.util import colors
 from cloudbot.util.formatting import pluralize
+from cloudbot.util.pager import paginated_list
 
-search_pages = defaultdict(list)
-search_page_indexes = {}
+search_pages = defaultdict(dict)
 
 user_url = "http://reddit.com/user/{}/"
 subreddit_url = "http://reddit.com/r/{}/"
 # This agent should be unique for your cloudbot instance
 agent = {"User-Agent": "gonzobot a cloudbot (IRCbot) implementation for snoonet.org by /u/bloodygonzo"}
-
-
-def two_lines(bigstring, chan):
-    """Receives a string with new lines. Groups the string into a list of strings with up to 2 new lines per string element. Returns first string element then stores the remaining list in search_pages."""
-    global search_pages
-    temp = bigstring.split('\n')
-    for i in range(0, len(temp), 2):
-        search_pages[chan].append('\n'.join(temp[i:i+2]))
-    search_page_indexes[chan] = 0
-    return search_pages[chan][0]
-
-
-def smart_truncate(content, length=355, suffix='...\n'):
-    if len(content) <= length:
-        return content
-    else:
-        return content[:length].rsplit(' \u2022 ', 1)[0]+ suffix + content[:length].rsplit(' \u2022 ', 1)[1] + smart_truncate(content[length:])
 
 
 def statuscheck(status, item):
@@ -49,34 +33,34 @@ def statuscheck(status, item):
 
 
 @hook.command("moremod", autohelp=False)
-def moremod(text, chan):
+def moremod(text, chan, conn):
     """if a sub or mod list has lots of results the results are pagintated. If the most recent search is paginated the pages are stored for retreival. If no argument is given the next page will be returned else a page number can be specified."""
-    if not search_pages[chan]:
+    chan_cf = chan.casefold()
+    pages = search_pages[conn.name].get(chan_cf)
+    if not pages:
         return "There are modlist pages to show."
     if text:
         try:
             index = int(text)
         except ValueError:
             return "Please specify an integer value."
-        if abs(index) > len(search_pages[chan]) or index == 0:
+        page = pages[index - 1]
+        if page is None:
             return "please specify a valid page number between 1 and {}.".format(len(search_pages[chan]))
         else:
-            return "{}(page {}/{})".format(search_pages[chan][index-1], index, len(search_pages[chan]))
+            return page
     else:
-        search_page_indexes[chan] += 1
-        if search_page_indexes[chan] < len(search_pages[chan]):
-            return "{}(page {}/{})".format(search_pages[chan][search_page_indexes[chan]], search_page_indexes[chan] + 1, len(search_pages[chan]))
+        page = pages.next()
+        if page is not None:
+            return page
         else:
             return "All pages have been shown."
 
 
 @hook.command("subs", "moderates", singlethread=True)
-def moderates(text, chan):
+def moderates(text, chan, conn):
     """This plugin prints the list of subreddits a user moderates listed in a reddit users profile. Private subreddits will not be listed."""
     #This command was written using concepts from FurCode http://github.com/FurCode.
-    global search_pages
-    search_pages[chan] = []
-    search_page_indexes[chan] = 0
     user = text
     r = requests.get(user_url.format(user), headers=agent)
     if r.status_code != 200:
@@ -89,14 +73,14 @@ def moderates(text, chan):
     mod_list = mod_list.text.split('r/')
     del mod_list[0]
     out = colors.parse("$(b){}$(b) moderates these public subreddits: ".format(user))
-    for sub in mod_list:
-        out += "{} \u2022 ".format(sub)
-    out = out[:-2]
-    out = smart_truncate(out)
-    if len(out.split('\n')) > 2:
-        out = two_lines(out, chan)
-        return "{}(page {}/{}) .moremod".format(out, search_page_indexes[chan] + 1, len(search_pages[chan]))
-    return out
+    pager = paginated_list(mod_list)
+    search_pages[conn.name][chan.casefold()] = pager
+    page = pager.next()
+    if len(page) > 1:
+        page[-1] += " .moremod"
+
+    page[0] = out + page[0]
+    return page
 
 
 @hook.command("karma", "ruser", singlethread=True)
@@ -160,11 +144,8 @@ def time_format(numdays):
 
 
 @hook.command("submods", "mods", "rmods", singlethread=True)
-def submods(text, chan):
+def submods(text, chan, conn):
     """submods <subreddit> prints the moderators of the specified subreddit."""
-    global search_pages
-    search_pages[chan] = []
-    search_page_indexes[chan] = 0
     sub = text
     if sub.startswith('/r/'):
         sub = sub[3:]
@@ -175,20 +156,24 @@ def submods(text, chan):
     if r.status_code != 200:
         return statuscheck(r.status_code, 'r/'+sub)
     data = r.json()
-    out = colors.parse("/r/$(b){}$(b) mods: ".format(sub))
+    moderators = []
     for mod in data['data']['children']:
         username = mod['name']
         # Showing the modtime makes the message too long for larger subs
         # if you want to show this information add modtime.days to out below
         modtime = datetime.now() - datetime.fromtimestamp(mod['date'])
         modtime = time_format(modtime.days)
-        out += "{} ({}{}) \u2022 ".format(username, modtime[0], modtime[1])
-    out = smart_truncate(out)
-    out = out[:-3]
-    if len(out.split('\n')) > 2:
-        out = two_lines(out, chan)
-        return "{}(page {}/{}) .moremod".format(out, search_page_indexes[chan] + 1, len(search_pages[chan]))
-    return out
+        moderators.append("{} ({}{})".format(username, modtime[0], modtime[1]))
+    pager = paginated_list(moderators)
+    search_pages[conn.name][chan.casefold()] = pager
+    page = pager.next()
+    if len(page) > 1:
+        page[-1] += " .moremod"
+
+    out = colors.parse("/r/$(b){}$(b) mods: ".format(sub))
+    page[0] = out + page[0]
+
+    return page
 
 
 @hook.command("subinfo","subreddit", "sub", "rinfo", singlethread=True)
