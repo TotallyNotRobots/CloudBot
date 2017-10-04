8d0f0eec73d3b9998389555da8b6f7b270f3b2c6
linuxdaemon <linuxdaemon@snoonet.org>
2017-10-04 18:58:21 -0500

    Add hook to match common bot command chars

--- plugins/core_out.py
+++ plugins/core_out.py
@@ -4,6 +4,7 @@ Core filters for IRC raw lines
 
 from cloudbot import hook
 from cloudbot.hook import Priority
+from cloudbot.util import colors
 
 NEW_LINE_TRANS_TBL = str.maketrans({
     '\r': None,
@@ -41,3 +42,15 @@ def encode_line(line, conn):
     encoding = conn.config.get("encoding", "utf-8")
     errors = conn.config.get("encoding_errors", "replace")
     return line.encode(encoding, errors)
+
+
+@hook.irc_out(priority=Priority.HIGH)
+def strip_command_chars(parsed_line, conn, line):
+    chars = conn.config.get("strip_cmd_chars", "!.@;$")
+    if chars and parsed_line and parsed_line.command == "PRIVMSG" and parsed_line.parameters[-1][0] in chars:
+        new_msg = colors.parse("$(red)[!!]$(clear) ") + parsed_line.parameters[-1]
+        parsed_line.parameters[-1] = new_msg
+        parsed_line.has_trail = True
+        return parsed_line
+
+    return line
