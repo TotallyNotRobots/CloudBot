795a41a05953b9031da664dee84935c128e0716d
linuxdaemon <linuxdaemon@snoonet.org>
2017-11-10 21:15:16 -0600

    Preserve key order when formatting json files

--- format_json.py
+++ format_json.py
@@ -3,6 +3,7 @@ Format all JSON files in the bot core in a consistent manor
 """
 
 from pathlib import Path
+import collections
 import codecs
 import json
 
@@ -11,7 +12,7 @@ path = Path().resolve()
 for file in path.rglob("*.json"):
     print(file)
     with file.open(encoding='utf8') as f:
-        data = json.load(f)
+        data = json.load(f, object_pairs_hook=collections.OrderedDict)
 
     with file.open('w', encoding='utf8') as f:
         print(json.dumps(data, indent=4), file=f)
