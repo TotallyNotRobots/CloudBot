0a0691c53556138c02b55d78cc955fca9063ddd8
IlGnome <IlGnome@users.noreply.github.com>
2017-11-17 23:24:29 -0600

    More pep8 stuff
    Because linuxdaemon won't stop yelling at me.

--- plugins/reactions.py
+++ plugins/reactions.py
@@ -15,34 +15,34 @@ deal_with_it_phrases = [ 'Stop complaining, \x02{}\x02, and',
 
 @hook.on_start()
 def load_macros(bot):
-    global reactionmacros
-    with codecs.open(os.path.join(bot.data_dir, "reactionmacros.json"), encoding="utf-8") as macros:
-        reactionmacros = json.load(macros)
+    global reaction_macros
+    with codecs.open(os.path.join(bot.data_dir, "reaction_macros.json"), encoding="utf-8") as macros:
+        reaction_macros = json.load(macros)
 
         
 @hook.command('dwi', 'dealwithit')
 def deal_with_it(text, message):
     """Tell some one in the channel to deal with it. File located in reactions.py"""
-    PersonNeedsToDeal = text.strip()
-    message('{} {}'.format(random.choice(deal_with_it_phrases).format(PersonNeedsToDeal), random.choice(reactionmacros['DWImacros'])))
+    person_needs_to_deal = text.strip()
+    message('{} {}'.format(random.choice(deal_with_it_phrases).format(person_needs_to_deal), random.choice(reaction_macros['DWImacros'])))
 
 
 @hook.command('fp', 'facepalm')
-def face_palm(text,message):
+def face_palm(text, message):
     """Expresses your frustration with another user. File located in reactions.py"""
-    FacePalmer = text.strip()
-    message('Dammit {} {}'.format(FacePalmer, random.choice(reactionmacros['Facepalmmacros'])))
+    face_palmer = text.strip()
+    message('Dammit {} {}'.format(FacePalmer, random.choice(reaction_macros['Facepalmmacros'])))
 
     
 @hook.command('hd', 'headdesk')
 def head_desk(nick, message):
     """Hit your head against the desk becausae of a user. Code located in reactions.py"""
     idiot = nick.strip()
-    message('{} {}'.format(idiot, random.choice(reactionmacros['HeadDeskMacros'])))
+    message('{} {}'.format(idiot, random.choice(reaction_macros['HeadDeskMacros'])))
 
     
 @hook.command('fetish', 'tmf')
-def my_fetish(nick, message):
+def my_fetish(text, message):
     """Did some one just mention what your fetish was? Let them know! Code located in reactions.py"""
-    PersonToShareFetishWith = nick.strip()
-    message('{} {}'.format(PersonToShareFetishWith, random.choice(reactionmacros['Fetishmacros'])))
+    person_to_share_fetish_with = nick.strip()
+    message('{} {}'.format(person_to_share_fetish_with, random.choice(reaction_macros['Fetishmacros'])))
