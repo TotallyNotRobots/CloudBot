151f4597aad94bc10344bba7cc717a12497578b6
IlGnome <IlGnome@users.noreply.github.com>
2017-11-17 23:37:57 -0600

    commiting more pep8 changes
    I hope linuxdaemon chokes on their pizza. Not like chokes to death, but that annoying 'down the wrong pipe' kind of choke.

--- plugins/reactions.py
+++ plugins/reactions.py
@@ -22,27 +22,27 @@ def load_macros(bot):
         
 @hook.command('dwi', 'dealwithit')
 def deal_with_it(text, message):
-    """Tell some one in the channel to deal with it. File located in reactions.py"""
+    """Tell <nick> in the channel to deal with it. Code located in reactions.py"""
     person_needs_to_deal = text.strip()
     message('{} {}'.format(random.choice(deal_with_it_phrases).format(person_needs_to_deal), random.choice(reaction_macros['DWImacros'])))
 
 
 @hook.command('fp', 'facepalm')
 def face_palm(text, message):
-    """Expresses your frustration with another user. File located in reactions.py"""
+    """Expresses your frustration with <Nick>. Code located in reactions.py"""
     face_palmer = text.strip()
     message('Dammit {} {}'.format(FacePalmer, random.choice(reaction_macros['Facepalmmacros'])))
 
     
 @hook.command('hd', 'headdesk')
-def head_desk(nick, message):
-    """Hit your head against the desk becausae of a user. Code located in reactions.py"""
-    idiot = nick.strip()
+def head_desk(text, message):
+    """Hit your head against the desk becausae of <nick>. Code located in reactions.py"""
+    idiot = text.strip()
     message('{} {}'.format(idiot, random.choice(reaction_macros['HeadDeskMacros'])))
 
     
 @hook.command('fetish', 'tmf')
 def my_fetish(text, message):
-    """Did some one just mention what your fetish was? Let them know! Code located in reactions.py"""
-    person_to_share_fetish_with = nick.strip()
+    """Did some one just mention what your fetish was? Let <nick> know! Code located in reactions.py"""
+    person_to_share_fetish_with = text.strip()
     message('{} {}'.format(person_to_share_fetish_with, random.choice(reaction_macros['Fetishmacros'])))
