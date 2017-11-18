f6c6bbfc1ef31332609b3055399936e928334d4b
linuxdaemon <linuxdaemon@users.noreply.github.com>
2017-11-17 23:13:52 -0600

    Add documentation to the mock command

--- plugins/mock.py
+++ plugins/mock.py
@@ -11,16 +11,18 @@ def get_latest_line(conn, chan, nick):
 
 @hook.command
 def mock(text, chan, conn, message):
+    """<nick> - turn <user>'s last message in to aLtErNaTiNg cApS"""
     nick = text.strip()
     line = get_latest_line(conn, chan, nick)
     if line is None:
         return "Nothing found in recent history for {}".format(nick)
 
     if line.startswith("\x01ACTION"):
-        fmt = "* {} {}"
+        fmt = "* {nick} {msg}"
         line = line[8:].strip(' \x01')
     else:
-        fmt = "<{}> {}"
+        fmt = "<{nick}> {msg}"
 
-    line = "".join(c.upper() if i & 1 else c for i, c in enumerate(line))
-    message(fmt.format(nick, line))
+    # Return the message in aLtErNaTiNg cApS
+    line = "".join(c.upper() if i & 1 else c.lower() for i, c in enumerate(line))
+    message(fmt.format(nick=nick, msg=line))
