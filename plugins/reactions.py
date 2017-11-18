1bd36223e273622835c1313553a074b05a009826
IlGnome <IlGnome@users.noreply.github.com>
2017-11-18 00:12:41 -0600

    More code clean up.
    Now I'm just doing it to pad my stats.

--- plugins/reactions.py
+++ plugins/reactions.py
@@ -21,27 +21,29 @@ def load_macros(bot):
         
 @hook.command('dwi', 'dealwithit')
 def deal_with_it(text, message):
-    """<nick>- Tell <nick> in the channel to deal with it. Code located in reactions.py"""
+    """<nick> - Tell <nick> in the channel to deal with it. Code located in reactions.py"""
     person_needs_to_deal = text.strip()
-    message('{} {}'.format(random.choice(deal_with_it_phrases).format(person_needs_to_deal), random.choice(reaction_macros['deal_with_it_macros'])))
+    phrase = random.choice(deal_with_it_phrases)
+    formated_phrase = phrase.format(text.strip())
+    message('{} {}'.format(formated_phrase, random.choice(reaction_macros['deal_with_it_macros'])))
 
 
 @hook.command('fp', 'facepalm')
 def face_palm(text, message):
-    """<nick>- Expresses your frustration with <Nick>. Code located in reactions.py"""
+    """<nick> - Expresses your frustration with <Nick>. Code located in reactions.py"""
     face_palmer = text.strip()
     message('Dammit {} {}'.format(FacePalmer, random.choice(reaction_macros['facepalm_macros'])))
 
     
 @hook.command('hd', 'headdesk')
 def head_desk(text, message):
-    """<nick>- Hit your head against the desk becausae of <nick>. Code located in reactions.py"""
+    """<nick> - Hit your head against the desk becausae of <nick>. Code located in reactions.py"""
     idiot = text.strip()
     message('{} {}'.format(idiot, random.choice(reaction_macros['head_desk_macros'])))
 
     
 @hook.command('fetish', 'tmf')
 def my_fetish(text, message):
-    """<nick>- Did some one just mention what your fetish was? Let <nick> know! Code located in reactions.py"""
+    """<nick> - Did some one just mention what your fetish was? Let <nick> know! Code located in reactions.py"""
     person_to_share_fetish_with = text.strip()
     message('{} {}'.format(person_to_share_fetish_with, random.choice(reaction_macros['fetish_macros'])))
