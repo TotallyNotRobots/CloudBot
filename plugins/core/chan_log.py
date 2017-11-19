b60e8bc8a5b872c93bfab3e8e25f538c000b1dca
linuxdaemon <linuxdaemon@snoonet.org>
2017-11-19 06:36:27 -0600

    Add Event object dump to hook error reporting

--- plugins/core/chan_log.py
+++ plugins/core/chan_log.py
@@ -7,7 +7,7 @@ logchannel = ""
 
 
 @hook.post_hook
-def on_hook_end(error, message, launched_hook):
+def on_hook_end(error, message, launched_hook, launched_event):
     if error is not None and logchannel:
         message("Error occurred in {}.{}".format(launched_hook.plugin.title, launched_hook.function_name), logchannel)
 
@@ -15,4 +15,9 @@ def on_hook_end(error, message, launched_hook):
         last_line = lines[-1]
         message(last_line, logchannel)
         url = web.paste('\n'.join(lines))
-        message(url, logchannel)
+        message("Traceback: " + url, logchannel)
+
+        event_data = launched_event.__dict__.items()
+        lines = ["{} = {}".format(k, v) for k, v in event_data]
+        url = web.paste('\n'.join(lines))
+        message("Event: " + url, logchannel)
