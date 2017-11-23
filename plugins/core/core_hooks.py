85c414535bc5508f52377bf273e7c8a478621341
linuxdaemon <linuxdaemon@snoonet.org>
2017-11-22 21:59:03 -0600

    Make sure core.core_hooks.do_reply ignores sieves and start/stop hooks

--- plugins/core/core_hooks.py
+++ plugins/core/core_hooks.py
@@ -13,6 +13,9 @@ def cmd_autohelp(bot, event, _hook):
 
 @hook.post_hook(priority=Priority.LOWEST)
 def do_reply(result, error, launched_event):
+    if launched_event.hook.type in ("sieve", "on_start", "on_stop"):
+        return
+
     if error is None and result is not None:
         if isinstance(result, (list, tuple)):
             # if there are multiple items in the response, return them on multiple lines
