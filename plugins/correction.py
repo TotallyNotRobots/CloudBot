7f6f9fc5ca3f894ddeb460eb9066c87d013c298c
linuxdaemon <linuxdaemon@snoonet.org>
2017-11-27 18:41:37 -0600

    Clean up correction logic

--- plugins/correction.py
+++ plugins/correction.py
@@ -23,8 +23,7 @@ def correction(match, conn, nick, chan, message):
     if not find.strip():  # Replacing empty or entirely whitespace strings is spammy
         return "really dude? you want me to replace nothing with {}?".format(replace)
 
-    for item in conn.history[chan].__reversed__():
-        name, timestamp, msg = item
+    for name, timestamp, msg in reversed(conn.history[chan]):
         if correction_re.match(msg):
             # don't correct corrections, it gets really confusing
             continue
@@ -32,18 +31,19 @@ def correction(match, conn, nick, chan, message):
         if find.lower() in msg.lower():
             find_esc = re.escape(find)
             replace_esc = re.escape(replace)
-            if "\x01ACTION" in msg:
-                msg = msg.replace("\x01ACTION", "").replace("\x01", "")
-                mod_msg = ireplace(msg, find_esc, "\x02" + replace_esc + "\x02")
-                message("Correction, * {} {}".format(name, mod_msg))
+            if msg.startswith('\x01ACTION'):
+                mod_msg = msg[7:].strip(' \x01')
+                fmt = "* {} {}"
             else:
-                mod_msg = ireplace(msg, find_esc, "\x02" + replace_esc + "\x02")
-                message("Correction, <{}> {}".format(name, mod_msg))
+                mod_msg = msg
+                fmt = "<{}> {}"
+
+            mod_msg = ireplace(mod_msg, find_esc, "\x02" + replace_esc + "\x02")
+
+            message("Correction, {}".format(fmt.format(name, mod_msg)))
 
             msg = ireplace(msg, find_esc, replace_esc)
             if nick.lower() == name.lower():
                 conn.history[chan].append((name, timestamp, msg))
-            return
-        else:
-            continue
-            # return("No matches for \"\x02{}\x02\" in recent messages from \x02{}\x02. You can only correct your own messages.".format(find, nick))
+
+            break
