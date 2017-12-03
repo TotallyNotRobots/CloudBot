ae6d44dd8e630b9565974b4f02f551524beff2e2
MuffinMedic <evan@muffinmedic.net>
2017-12-03 13:56:58 -0500

    Remove notice param

--- plugins/factoids.py
+++ plugins/factoids.py
@@ -184,7 +184,7 @@ def listfactoids(notice, chan):
 
 
 @hook.command("listdetailedfacts", autohelp=False)
-def listdetailedfactoids(notice, chan):
+def listdetailedfactoids(chan):
     """- lists all available factoids with their respective data"""
     formatted_facts = []
     for fact in sorted(factoid_cache[chan].items()):
