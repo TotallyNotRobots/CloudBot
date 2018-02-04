4be4c38585848b9010ecd6fa06aeb761200aad4a
linuxdaemon <linuxdaemon@snoonet.org>
2018-02-04 17:27:47 -0600

    Remove conncheck command due to it being broken

--- plugins/core/check_conn.py
+++ plugins/core/check_conn.py
@@ -5,21 +5,6 @@ from cloudbot import hook
 from cloudbot.util import colors
 
 
-@hook.command(autohelp=False, permissions=["botcontrol"])
-def conncheck(nick, bot, notice):
-    """- This command is an effort to make the bot reconnect to a network if it has been disconnected."""
-    # For each irc network return a notice on the connection state and send a message from
-    # each connection to the nick that used the command.
-    for conn in bot.connections:
-        # I am not sure if the connected property is ever changed.
-        notice("{}, {}".format(conn, bot.connections[conn].connected))
-        # if the value is in fact false try to connect
-        if not bot.connections[conn].connected:
-            bot.connections[conn].try_connect()
-        # Send a message from each irc network connection to the nick that issued the command
-        bot.connections[conn].message(nick, "just letting you know I am here. {}".format(conn))
-
-
 @asyncio.coroutine
 def do_reconnect(conn, auto=True):
     if conn.connected:
