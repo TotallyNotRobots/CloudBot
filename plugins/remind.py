92b6a1351166ccf731458b4c5fc2a865a41a90c7
linuxdaemon <linuxdaemon@snoonet.org>
2018-03-31 04:56:27 -0500

    Update remind.py to use Event.db_session

--- plugins/remind.py
+++ plugins/remind.py
@@ -11,7 +11,6 @@ License:
     GPL v3
 """
 
-import asyncio
 import time
 from datetime import datetime
 
@@ -35,27 +34,26 @@ table = Table(
 )
 
 
-@asyncio.coroutine
-def delete_reminder(async_call, db, network, remind_time, user):
+def delete_reminder(event, network, remind_time, user):
     query = table.delete() \
         .where(table.c.network == network.lower()) \
         .where(table.c.remind_time == remind_time) \
         .where(table.c.added_user == user.lower())
-    yield from async_call(db.execute, query)
-    yield from async_call(db.commit)
+    with event.db_session() as db:
+        db.execute(query)
+        db.commit()
 
 
-@asyncio.coroutine
-def delete_all(async_call, db, network, user):
+def delete_all(event, network, user):
     query = table.delete() \
         .where(table.c.network == network.lower()) \
         .where(table.c.added_user == user.lower())
-    yield from async_call(db.execute, query)
-    yield from async_call(db.commit)
+    with event.db_session() as db:
+        db.execute(query)
+        db.commit()
 
 
-@asyncio.coroutine
-def add_reminder(async_call, db, network, added_user, added_chan, message, remind_time, added_time):
+def add_reminder(event, network, added_user, added_chan, message, remind_time, added_time):
     query = table.insert().values(
         network=network.lower(),
         added_user=added_user.lower(),
@@ -64,28 +62,26 @@ def add_reminder(async_call, db, network, added_user, added_chan, message, remin
         message=message,
         remind_time=remind_time
     )
-    yield from async_call(db.execute, query)
-    yield from async_call(db.commit)
+    with event.db_session() as db:
+        db.execute(query)
+        db.commit()
 
 
 @hook.on_start()
-@asyncio.coroutine
-def load_cache(async_call, db):
+def load_cache(event):
     global reminder_cache
     reminder_cache = []
 
-    for network, remind_time, added_time, user, message in (yield from async_call(_load_cache_db, db)):
-        reminder_cache.append((network, remind_time, added_time, user, message))
+    with event.db_session() as db:
+        rows = db.execute(table.select()).fetchall()
 
-
-def _load_cache_db(db):
-    query = db.execute(table.select())
-    return [(row["network"], row["remind_time"], row["added_time"], row["added_user"], row["message"]) for row in query]
+    for row in rows:
+        reminder_cache.append(
+            (row["network"], row["remind_time"], row["added_time"], row["added_user"], row["message"]))
 
 
 @hook.periodic(30, initial_interval=30)
-@asyncio.coroutine
-def check_reminders(bot, async_call, db):
+def check_reminders(bot, event):
     current_time = datetime.now()
 
     for reminder in reminder_cache:
@@ -93,8 +89,6 @@ def check_reminders(bot, async_call, db):
         if remind_time <= current_time:
             if network not in bot.connections:
                 # connection is invalid
-                yield from add_reminder(async_call, db, network, remind_time, user)
-                yield from load_cache(async_call, db)
                 continue
 
             conn = bot.connections[network]
@@ -115,13 +109,12 @@ def check_reminders(bot, async_call, db):
                        " it seems I was unable to deliver it on time)".format(late_time)
                 conn.message(user, colors.parse(late))
 
-            yield from delete_reminder(async_call, db, network, remind_time, user)
-            yield from load_cache(async_call, db)
+            delete_reminder(event, network, remind_time, user)
+            load_cache(event)
 
 
 @hook.command('remind', 'reminder', 'in')
-@asyncio.coroutine
-def remind(text, nick, chan, db, conn, notice, async_call):
+def remind(text, nick, chan, conn, notice, event):
     """<1 minute, 30 seconds>: <do task> -- reminds you to <do task> in <1 minute, 30 seconds>"""
 
     count = len([x for x in reminder_cache if x[0] == conn.name and x[3] == nick.lower()])
@@ -130,8 +123,8 @@ def remind(text, nick, chan, db, conn, notice, async_call):
         if count == 0:
             return "You have no reminders to delete."
 
-        yield from delete_all(async_call, db, conn.name, nick)
-        yield from load_cache(async_call, db)
+        delete_all(event, conn.name, nick)
+        load_cache(event)
         return "Deleted all ({}) reminders for {}!".format(count, nick)
 
     # split the input on the first ":"
@@ -167,8 +160,8 @@ def remind(text, nick, chan, db, conn, notice, async_call):
         return "I can't remind you in the past!"
 
     # finally, add the reminder and send a confirmation message
-    yield from add_reminder(async_call, db, conn.name, nick, chan, message, remind_time, current_time)
-    yield from load_cache(async_call, db)
+    add_reminder(event, conn.name, nick, chan, message, remind_time, current_time)
+    load_cache(event)
 
     remind_text = format_time(seconds, count=2)
     output = "Alright, I'll remind you \"{}\" in $(b){}$(clear)!".format(message, remind_text)
