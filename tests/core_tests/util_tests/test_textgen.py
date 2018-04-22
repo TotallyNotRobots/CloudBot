6af778919678001e9c1e5c58c67e56077e50b8c7
linuxdaemon <linuxdaemon@snoonet.org>
2018-04-22 15:51:04 -0500

    Add more tests for text generation

--- tests/core_tests/util_tests/test_textgen.py
+++ tests/core_tests/util_tests/test_textgen.py
@@ -8,3 +8,26 @@ def test_basic_format():
     )
 
     generator.generate_strings(100)
+
+
+def test_get_template():
+    from cloudbot.util.textgen import TextGenerator
+    generator = TextGenerator(
+        ["{test} thing {other} {third}"],
+        {"test": ["a", ("b", 10)]},
+        [0],
+        {"other": "a"}
+    )
+    assert generator.get_template(0) == "{test} thing {other} {third}"
+
+
+def test_no_defaults():
+    from cloudbot.util.textgen import TextGenerator
+    generator = TextGenerator(
+        ["{test} thing {other} {third}"],
+        {"test": ["a", ("b", 10)]},
+        [],
+        {"other": "a"}
+    )
+
+    generator.generate_strings(100)
