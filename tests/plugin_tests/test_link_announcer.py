8eb9fea7013bf6dfea055cae2c505f0c34dd273b
linuxdaemon <linuxdaemon@snoonet.org>
2018-04-23 15:42:11 -0500

    Clean up link announcer tests

--- tests/plugin_tests/test_link_announcer.py
+++ tests/plugin_tests/test_link_announcer.py
@@ -3,7 +3,43 @@ import codecs
 import pytest
 from bs4 import BeautifulSoup
 
-MATCHES = (
+
+@pytest.mark.parametrize("url", [
+    "http://",
+    "http://.",
+    "http://..",
+    "http://?",
+    "http://??",
+    "http://??/",
+    "http://#",
+    "http://##",
+    "http://##/",
+    "http://foo.bar?q=Spaces should be encoded",
+    "//",
+    "//a",
+    "///a",
+    "///",
+    "http:///a",
+    "foo.com",
+    "rdar://1234",
+    "h://test",
+    "http:// shouldfail.com",
+    ":// should fail",
+    "http://foo.bar/foo(bar)baz quux",
+    "ftps://foo.bar/",
+    "https://foo.bar/baz.ext)",
+    "https://foo.bar/test.",
+    "https://foo.bar/test(test",
+    "https://foo.bar.",
+    "https://foo.bar./",
+])
+def test_url_fails(url):
+    from plugins.link_announcer import url_re
+    match = url_re.fullmatch(url)
+    assert not match, match.group()
+
+
+@pytest.mark.parametrize("url", [
     "http://foo.com/blah_blah",
     "http://foo.com/blah_blah/",
     "http://foo.com/blah_blah_(wikipedia)",
@@ -32,75 +68,38 @@ MATCHES = (
     "http://223.255.255.254",
     "https://foo.bar/baz?#",
     "https://foo.bar/baz?",
-)
-
-FAILS = (
-    "http://",
-    "http://.",
-    "http://..",
-    "http://?",
-    "http://??",
-    "http://??/",
-    "http://#",
-    "http://##",
-    "http://##/",
-    "http://foo.bar?q=Spaces should be encoded",
-    "//",
-    "//a",
-    "///a",
-    "///",
-    "http:///a",
-    "foo.com",
-    "rdar://1234",
-    "h://test",
-    "http:// shouldfail.com",
-    ":// should fail",
-    "http://foo.bar/foo(bar)baz quux",
-    "ftps://foo.bar/",
-    "https://foo.bar/baz.ext)",
-    "https://foo.bar/test.",
-    "https://foo.bar/test(test",
-    "https://foo.bar.",
-    "https://foo.bar./",
-)
-
-SEARCH = (
-    ("(https://foo.bar)", "https://foo.bar"),
-    ("[https://example.com]", "https://example.com"),
-    ("<a hreh=\"https://example.com/test.page?#test\">", "https://example.com/test.page?#test"),
-    ("<https://www.example.com/this.is.a.test/blah.txt?a=1#123>",
-     "https://www.example.com/this.is.a.test/blah.txt?a=1#123"),
-)
-
-
-@pytest.mark.parametrize("url", list(FAILS))
-def test_url_fails(url):
-    from plugins.link_announcer import url_re
-    match = url_re.fullmatch(url)
-    assert not match, match.group()
-
-
-@pytest.mark.parametrize("url", list(MATCHES))
+])
 def test_url_matches(url):
     from plugins.link_announcer import url_re
     assert url_re.fullmatch(url), url
 
 
-@pytest.mark.parametrize("text,out", list(SEARCH))
+@pytest.mark.parametrize("text,out", [
+    ("(https://foo.bar)", "https://foo.bar"),
+    ("(https://foo.bar/baz?bing=a)", "https://foo.bar/baz?bing=a"),
+    (
+        "Comparing gonzobot...gonzobot-2.0 · snoonetIRC/CloudBot "
+        "(https://github.com/snoonetIRC/CloudBot/compare/gonzobot...gonzobot-2.0)",
+        "https://github.com/snoonetIRC/CloudBot/compare/gonzobot...gonzobot-2.0"
+    ),
+    ("[https://example.com]", "https://example.com"),
+    ("<a href=\"https://example.com/test.page?#test\">", "https://example.com/test.page?#test"),
+    (
+        "<https://www.example.com/this.is.a.test/blah.txt?a=1#123>",
+        "https://www.example.com/this.is.a.test/blah.txt?a=1#123"
+    ),
+])
 def test_search(text, out):
     from plugins.link_announcer import url_re
     match = url_re.search(text)
     assert match and match.group() == out
 
 
-ENCODINGS = (
+@pytest.mark.parametrize("text,enc", [
     (b'<meta charset="utf8">', codecs.lookup('utf8')),
     (b'', None),
     (b'<meta http-equiv="Content-Type" content="text/html; charset=utf-8">', codecs.lookup('utf8')),
-)
-
-
-@pytest.mark.parametrize("text,enc", list(ENCODINGS))
+])
 def test_encoding_parse(text, enc):
     from plugins.link_announcer import get_encoding
     soup = BeautifulSoup(text, "lxml")
