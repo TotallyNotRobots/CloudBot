2bf9943209f6e239a2f4308e4e7c25f2815c6981
Marcus Leivo <macleivo@users.noreply.github.com>
2018-07-12 15:33:04 +0300

    Exclude FML photos
    Need to manually exclude FML photos.

--- plugins/mylife.py
+++ plugins/mylife.py
@@ -29,8 +29,9 @@ def refresh_fml_cache(loop):
         # the .html in the url must be removed before extracting the id
         fml_id = int(a['href'][:-5].split('_')[-1])
         text = a.text.strip()
-        # exclude lengthy submissions
-        if len(text) > 375:
+        
+        # exclude lengthy submissions and FML photos
+        if len(text) > 375 or text[-3:].lower() != "fml":
             continue
         fml_cache.append((fml_id, text))
 
