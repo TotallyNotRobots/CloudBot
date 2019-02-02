95d511500a812faf458409d9842674f0a315087e
linuxdaemon <linuxdaemon@snoonet.org>
2019-02-02 12:50:42 -0600

    Make sure to reload modules prior to tests

--- tests/core_tests/util_tests/test_database.py
+++ tests/core_tests/util_tests/test_database.py
@@ -1,6 +1,9 @@
-from cloudbot.util.database import metadata, base
 
 
 def test_database():
+    import sys
+    # Make sure we start with a fresh module
+    sys.modules.pop('cloudbot.util.database', None)
+    from cloudbot.util.database import metadata, base
     assert metadata is None
     assert base is None
