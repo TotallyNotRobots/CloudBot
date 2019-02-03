9559aae32c7f4c5bfc5d31fe5cbf5a3ed640a3db
linuxdaemon <linuxdaemon@snoonet.org>
2019-02-02 23:31:19 -0600

    Fix loading drinks.py data

--- plugins/drinks.py
+++ plugins/drinks.py
@@ -5,7 +5,7 @@ import random
 from cloudbot import hook
 from cloudbot.util import web
 
-drink_data = {}
+drink_data = []
 
 
 @hook.onload()
@@ -13,13 +13,13 @@ def load_drinks(bot):
     """load the drink recipes"""
     drink_data.clear()
     with open(os.path.join(bot.data_dir, "drinks.json")) as json_data:
-        drink_data.update(json.load(json_data))
+        drink_data.extend(json.load(json_data))
 
 
 @hook.command('drink')
 def drink_cmd(text, chan, action):
     """<nick> - makes the user a random cocktail."""
-    index = random.randint(0, len(drinks) - 1)
+    index = random.randint(0, len(drink_data) - 1)
     drink = drink_data[index]['title']
     url = web.try_shorten(drink_data[index]['url'])
     if drink.endswith(' recipe'):
