d671123963de6cc0b5059cd4ac37261e6189ed64
linuxdaemon <linuxdaemon@snoonet.org>
2019-02-10 07:05:36 -0600

    Remove unneeded closing()

--- plugins/link_announcer.py
+++ plugins/link_announcer.py
@@ -1,5 +1,4 @@
 import re
-from contextlib import closing
 
 import requests
 from bs4 import BeautifulSoup
@@ -89,7 +88,7 @@ def parse_content(content, encoding=None):
 
 @hook.regex(url_re, priority=Priority.LOW, action=Action.HALTTYPE, only_no_match=True)
 def print_url_title(message, match):
-    with closing(requests.get(match.group(), headers=HEADERS, stream=True, timeout=3)) as r:
+    with requests.get(match.group(), headers=HEADERS, stream=True, timeout=3) as r:
         if not r.encoding or not r.ok:
             return
 
