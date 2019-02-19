2e91405aa66072dfffa814f42305c748fe149ba8
linuxdaemon <linuxdaemon@snoonet.org>
2019-02-18 18:04:26 -0600

    Make test_json only test relevant data

--- tests/test_json.py
+++ tests/test_json.py
@@ -11,7 +11,10 @@ from pathlib import Path
 def pytest_generate_tests(metafunc):
     if 'json_file' in metafunc.fixturenames:
         paths = [
-            file for file in Path().rglob("*.json") if 'venv' not in file.parts
+            file for file in Path().rglob("*.json")
+            if len(file.parts) == 1 or file.parts[0] in (
+                'cloudbot', 'data', 'tests', 'travis', 'docs'
+            )
         ]
         metafunc.parametrize('json_file', paths, ids=list(map(str, paths)))
 
