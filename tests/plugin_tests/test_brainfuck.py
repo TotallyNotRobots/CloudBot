11f12d7b1511912176d15b39b9b518d7dc3022b9
linuxdaemon <linuxdaemon@snoonet.org>
2019-02-19 04:15:30 -0600

    Add tests for brainfuck interpreter

---
+++ tests/plugin_tests/test_brainfuck.py
@@ -0,0 +1,25 @@
+import asyncio
+
+import pytest
+
+
+@pytest.mark.parametrize('text,output', [
+    ('', 'No output'),
+    ('[', 'Unbalanced brackets'),
+    (']', 'Unbalanced brackets'),
+    (
+            '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..'
+            '+++.>>.<-.<.+++.------.--------.>>+.>++.',
+            'Hello World!'
+    ),
+    ('+[>.]+', 'No printable output'),
+    ('++++[>.]+', 'No printable output'),
+    ('++++[>++]+.', '(no output)(exceeded 1000000 iterations)'),
+    ('++++[>,]+.', '(no output)(exceeded 1000000 iterations)'),
+    ('[>,]>+.', 'No printable output'),
+    ('..' * 500, 'No printable output'),
+])
+def test_brainfuck(text, output):
+    from plugins.brainfuck import bf
+    loop = asyncio.get_event_loop()
+    assert loop.run_until_complete(bf(text)) == output
