3b2c131aef9c7d4e4da784493e7279a3792382fb
linuxdaemon <linuxdaemon@snoonet.org>
2019-02-18 19:10:52 -0600

    Add tests for googleurlparse

---
+++ tests/plugin_tests/test_googleurlparse.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.parametrize('text,url', [
+    ('www.google.com/url?thing&url=example.com', 'example.com'),
+])
+def test_google_url(text, url):
+    from plugins.googleurlparse import spamurl, google_url
+    match = spamurl.search(text)
+    assert match
+    assert google_url(match) == url
