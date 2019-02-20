ba85d6cee2aaf964ebee13d3452da081f91ad1c4
A_D <A_D@snoonet.org>
2019-02-20 22:08:39 +0200

    added link announcer tests

--- tests/plugin_tests/test_link_announcer.py
+++ tests/plugin_tests/test_link_announcer.py
@@ -1,8 +1,10 @@
 import codecs
 
 from bs4 import BeautifulSoup
-
-from plugins.link_announcer import url_re, get_encoding
+from responses import RequestsMock
+from mock import MagicMock
+from plugins.link_announcer import url_re, get_encoding, print_url_title
+import pytest
 
 MATCHES = (
     "http://foo.com/blah_blah",
@@ -107,3 +109,34 @@ def test_encoding_parse():
         enc_obj = codecs.lookup(encoding)
 
         assert enc, enc_obj
+
+
+STD_HTML = "<head><title>{}</title></head>"
+TESTS = {
+    "http://www.montypython.fun": ("<!DOCTYPE html><head><title>{}</title></head><body>test</body>", "This Site is dead."),
+    "http://www.talos.principle": (STD_HTML, "In the beginning were the words"),
+    "http://www.nonexistent.lol": ("", False),
+    "http://www.much-newlines.backslashn": (("\n" * 500) + STD_HTML, "new lines!"),
+    "http://completely.invalid": ("\x01\x01\x02\x03\x05\x08\x13", False),
+    "http://large.amounts.of.text": (STD_HTML + ("42"*512*4096) + "</body>", "here have a couple megs of text"),
+    "http://star.trek.the.next.title": (STD_HTML, "47" * 512 * 4096),
+    "http://bare.title": ("<title>{}</title>", "here has title")
+}
+
+
+@pytest.mark.parametrize(
+    "match,test_str,res",
+    [(url_re.search(a), b.format(c), c) for a, (b, c) in TESTS.items()]
+)
+def test_link_announce(match, test_str, res):
+    with RequestsMock() as reqs:
+        reqs.add(RequestsMock.GET, match.string, body=test_str, stream=True)
+        mck = MagicMock()
+
+        print_url_title(match=match, message=mck)
+        if res and len(test_str) < 1_000_000:
+            mck.assert_called_with("Title: \x02" + res + "\x02")
+        else:
+            mck.assert_not_called()
+
+
