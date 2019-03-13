3acad20056db75b8047f8e2d644aae813f0aee9a
linuxdaemon <linuxdaemon@snoonet.org>
2019-03-13 07:37:14 -0500

    Add tests for error handling in fact removal

--- tests/plugin_tests/test_factoids.py
+++ tests/plugin_tests/test_factoids.py
@@ -2,6 +2,7 @@ from textwrap import dedent
 
 import pytest
 from mock import patch, MagicMock, call
+from responses import RequestsMock
 
 
 def test_forget():
@@ -20,7 +21,36 @@ def patch_paste():
         yield mock
 
 
+def test_remove_fact_no_paste():
+    from plugins.factoids import factoid_cache
+    factoid_cache.clear()
+    with RequestsMock() as reqs:
+        reqs.add(reqs.POST, 'https://hastebin.com/documents', status=404)
+        mock_session = MagicMock()
+        mock_notice = MagicMock()
+
+        from plugins.factoids import remove_fact
+        remove_fact('#example', ['foo'], mock_session, mock_notice)
+        mock_notice.assert_called_once_with("Unknown factoids: 'foo'")
+
+        mock_session.execute.assert_not_called()
+
+        mock_notice.reset_mock()
+
+        factoid_cache['#example']['foo'] = 'bar'
+
+        remove_fact('#example', ['foo', 'bar'], mock_session, mock_notice)
+        mock_notice.assert_has_calls([
+            call("Unknown factoids: 'bar'"),
+            call('Unable to paste removed data, not removing facts'),
+        ])
+
+        mock_session.execute.assert_not_called()
+
+
 def test_remove_fact(patch_paste):
+    from plugins.factoids import factoid_cache
+    factoid_cache.clear()
     mock_session = MagicMock()
     mock_notice = MagicMock()
 
@@ -30,7 +60,6 @@ def test_remove_fact(patch_paste):
 
     mock_session.execute.assert_not_called()
 
-    from plugins.factoids import factoid_cache
     factoid_cache['#example']['foo'] = 'bar'
 
     patch_paste.return_value = "PASTEURL"
