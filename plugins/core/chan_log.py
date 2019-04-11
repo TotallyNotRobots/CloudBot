2bed469b5711e142158252c29959c6ad900fa5f0
linuxdaemon <linuxdaemon@snoonet.org>
2019-04-11 17:44:31 -0500

    Clean up line length issues in chan_log
    Lines longer than 80-85 chars become difficult to read for people (like
    me) who use a larger font size and window scale due to visual
    impairment. I plan to implement these line length limits project wide

--- plugins/core/chan_log.py
+++ plugins/core/chan_log.py
@@ -31,7 +31,9 @@ def format_error_data(exc):
         yield ''
         # Get "direct cause of" or
         # "during handling of ..., another exception occurred" stack
-        exc = getattr(exc, '__cause__', None) or getattr(exc, '__context__', None)
+        cause = getattr(exc, '__cause__', None)
+        context = getattr(exc, '__context__', None)
+        exc = cause or context
 
 
 def _dump_attrs(obj):
@@ -45,7 +47,9 @@ def on_hook_end(error, launched_hook, launched_event, admin_log):
     should_broadcast = True
     if error is not None:
         messages = [
-            "Error occurred in {}.{}".format(launched_hook.plugin.title, launched_hook.function_name)
+            "Error occurred in {}.{}".format(
+                launched_hook.plugin.title, launched_hook.function_name
+            )
         ]
 
         try:
@@ -54,17 +58,24 @@ def on_hook_end(error, launched_hook, launched_event, admin_log):
             messages.append(last_line.strip())
         except Exception:
             msg = traceback.format_exc()[-1]
-            messages.append("Error occurred while formatting error {}".format(msg))
+            messages.append(
+                "Error occurred while formatting error {}".format(msg)
+            )
         else:
             try:
                 url = web.paste('\n'.join(lines))
                 messages.append("Traceback: " + url)
             except Exception:
                 msg = traceback.format_exc()[-1]
-                messages.append("Error occurred while gathering traceback {}".format(msg))
+                messages.append(
+                    "Error occurred while gathering traceback {}".format(msg)
+                )
 
         try:
-            lines = ["{} = {!r}".format(k, v) for k, v in dump_attrs(launched_event)]
+            lines = [
+                "{} = {!r}".format(k, v)
+                for k, v in dump_attrs(launched_event)
+            ]
             _, exc, _ = error
 
             lines.append("")
@@ -76,19 +87,26 @@ def on_hook_end(error, launched_hook, launched_event, admin_log):
                     req = exc.request
                     lines.append("")
                     lines.append("Request Info:")
-                    lines.extend("{} = {}".format(k, v) for k, v in _dump_attrs(req))
+                    lines.extend(
+                        "{} = {!r}".format(k, v) for k, v in _dump_attrs(req)
+                    )
 
                 if exc.response is not None:
                     response = exc.response
                     lines.append("")
                     lines.append("Response Info:")
-                    lines.extend("{} = {}".format(k, v) for k, v in _dump_attrs(response))
+                    lines.extend(
+                        "{} = {!r}".format(k, v)
+                        for k, v in _dump_attrs(response)
+                    )
 
             url = web.paste('\n'.join(lines))
             messages.append("Event: " + url)
         except Exception:
             msg = traceback.format_exc()[-1]
-            messages.append("Error occurred while gathering error data {}".format(msg))
+            messages.append(
+                "Error occurred while gathering error data {}".format(msg)
+            )
 
         for message in messages:
             admin_log(message, should_broadcast)
