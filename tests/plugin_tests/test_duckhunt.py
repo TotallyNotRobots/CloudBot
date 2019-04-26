a6db6522e52c1fe9cc597f224ad3d026bf8a0bdb
linuxdaemon <linuxdaemon@snoonet.org>
2019-04-26 10:50:33 -0500

    Clean up duckhunt tests

--- tests/plugin_tests/test_duckhunt.py
+++ tests/plugin_tests/test_duckhunt.py
@@ -19,15 +19,15 @@ def mock_db():
 
 
 @pytest.mark.parametrize('prefix,items,result', [
-    (
-            'Duck friend scores in #TestChannel: ',
-            {
-                'testuser': 5,
-                'testuser1': 1,
-            },
-            'Duck friend scores in #TestChannel: '
-            '\x02t\u200bestuser\x02: 5 • \x02t\u200bestuser1\x02: 1'
-    ),
+    [
+        'Duck friend scores in #TestChannel: ',
+        {
+            'testuser': 5,
+            'testuser1': 1,
+        },
+        'Duck friend scores in #TestChannel: '
+        '\x02t\u200bestuser\x02: 5 • \x02t\u200bestuser1\x02: 1'
+    ],
 ])
 def test_top_list(prefix, items, result):
     assert top_list(prefix, items.items()) == result
@@ -49,60 +49,68 @@ def test_display_scores(mock_db):
 
     conn = Conn()
 
-    duckhunt.update_score('TestUser', '#TestChannel', session, conn, 5, 4)
-    duckhunt.update_score('TestUser1', '#TestChannel', session, conn, 1, 7)
-    duckhunt.update_score('OtherUser', '#TestChannel1', session, conn, 9, 2)
+    chan = '#TestChannel'
+    chan1 = '#TestChannel1'
 
-    assert duckhunt.get_channel_scores(
-        session, duckhunt.SCORE_TYPES['friend'], conn, '#TestChannel'
-    ) == {'testuser': 4, 'testuser1': 7}
+    duckhunt.update_score('TestUser', chan, session, conn, 5, 4)
+    duckhunt.update_score('TestUser1', chan, session, conn, 1, 7)
+    duckhunt.update_score('OtherUser', chan1, session, conn, 9, 2)
 
-    assert repr(
-        duckhunt.friends('', '#TestChannel', conn, session)
-    ) == repr(
+    expected_testchan_friend_scores = {'testuser': 4, 'testuser1': 7}
+
+    actual_testchan_friend_scores = duckhunt.get_channel_scores(
+        session, duckhunt.SCORE_TYPES['friend'],
+        conn, chan
+    )
+
+    assert actual_testchan_friend_scores == expected_testchan_friend_scores
+
+    chan_friends = (
         'Duck friend scores in #TestChannel: '
         '\x02t\u200bestuser1\x02: 7 • \x02t\u200bestuser\x02: 4'
     )
 
-    assert repr(
-        duckhunt.killers('', '#TestChannel', conn, session)
-    ) == repr(
+    chan_kills = (
         'Duck killer scores in #TestChannel: '
         '\x02t\u200bestuser\x02: 5 • \x02t\u200bestuser1\x02: 1'
     )
 
-    assert repr(
-        duckhunt.friends('global', '#TestChannel', conn, session)
-    ) == repr(
+    global_friends = (
         'Duck friend scores across the network: '
         '\x02t\u200bestuser1\x02: 7'
         ' • \x02t\u200bestuser\x02: 4'
         ' • \x02o\u200btheruser\x02: 2'
     )
 
-    assert repr(
-        duckhunt.killers('global', '#TestChannel', conn, session)
-    ) == repr(
+    global_kills = (
         'Duck killer scores across the network: '
         '\x02o\u200btheruser\x02: 9'
         ' • \x02t\u200bestuser\x02: 5'
         ' • \x02t\u200bestuser1\x02: 1'
     )
 
-    assert repr(
-        duckhunt.friends('average', '#TestChannel', conn, session)
-    ) == repr(
+    average_friends = (
         'Duck friend scores across the network: '
         '\x02t\u200bestuser1\x02: 7'
         ' • \x02t\u200bestuser\x02: 4'
         ' • \x02o\u200btheruser\x02: 2'
     )
 
-    assert repr(
-        duckhunt.killers('average', '#TestChannel', conn, session)
-    ) == repr(
+    average_kills = (
         'Duck killer scores across the network: '
         '\x02o\u200btheruser\x02: 9'
         ' • \x02t\u200bestuser\x02: 5'
         ' • \x02t\u200bestuser1\x02: 1'
     )
+
+    assert duckhunt.friends('', chan, conn, session) == chan_friends
+
+    assert duckhunt.killers('', chan, conn, session) == chan_kills
+
+    assert duckhunt.friends('global', chan, conn, session) == global_friends
+
+    assert duckhunt.killers('global', chan, conn, session) == global_kills
+
+    assert duckhunt.friends('average', chan, conn, session) == average_friends
+
+    assert duckhunt.killers('average', chan, conn, session) == average_kills
