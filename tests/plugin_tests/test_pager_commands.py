5f2a547c68db8637765f92d7f756104940edbec2
linuxdaemon <linuxdaemon@snoonet.org>
2019-04-28 17:23:01 -0500

    Add profile.py pager tests

--- tests/plugin_tests/test_pager_commands.py
+++ tests/plugin_tests/test_pager_commands.py
@@ -55,3 +55,58 @@ def test_page_commands(plugin_name, hook_name, pages_name, page_type):
     assert hook('3', '#testchannel', conn) == out_of_range
 
     assert hook('a', '#testchannel', conn) == no_number
+
+
+class CaptureCalls:
+    def __init__(self):
+        self.lines = []
+
+    def __call__(self, text):
+        self.lines.append(text)
+
+
+def test_profile_pager():
+    from plugins import profile
+
+    hook = profile.moreprofile
+
+    pages = profile.cat_pages
+
+    conn = MockConn('testconn')
+
+    def call(*args):
+        notice = CaptureCalls()
+        hook(*args, notice=notice)
+        return notice.lines
+
+    no_grabs = "There are no category pages to show."
+    done = "All pages have been shown. " \
+           "You can specify a page number or do a new search."
+    out_of_range = "Please specify a valid page number between 1 and 2."
+    no_number = "Please specify an integer value."
+
+    assert call('', '#testchannel', 'testuser') == [no_grabs]
+
+    pages['#testchannel1']['testuser1'] = Pager(['a', 'b', 'c'])
+
+    assert call('', '#testchannel', 'testuser') == [no_grabs]
+
+    pages['#testchannel']['testuser1'] = Pager(['a', 'b', 'c'])
+
+    assert call('', '#testchannel', 'testuser') == [no_grabs]
+
+    pages['#testchannel1']['testuser'] = Pager(['a', 'b', 'c'])
+
+    assert call('', '#testchannel', 'testuser') == [no_grabs]
+
+    pages['#testchannel']['testuser'] = Pager(['a', 'b', 'c'])
+
+    assert call('', '#testchannel', 'testuser') == ['a', 'b (page 1/2)']
+    assert call('', '#testchannel', 'testuser') == ['c (page 2/2)']
+    assert call('', '#testchannel', 'testuser') == [done]
+
+    assert call('1', '#testchannel', 'testuser') == ['a', 'b (page 1/2)']
+    assert call('2', '#testchannel', 'testuser') == ['c (page 2/2)']
+    assert call('3', '#testchannel', 'testuser') == [out_of_range]
+
+    assert call('a', '#testchannel', 'testuser') == [no_number]
