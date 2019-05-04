809920e3668bbe233848eddde751631ed4299f85
linuxdaemon <linuxdaemon@snoonet.org>
2019-05-04 18:16:47 -0500

    vimeo.py: Use .format() instead of %

--- plugins/vimeo.py
+++ plugins/vimeo.py
@@ -2,11 +2,11 @@ from cloudbot import hook
 from cloudbot.util import http, timeformat
 
 
-@hook.regex(r'vimeo.com/([0-9]+)')
+@hook.regex(r"vimeo.com/([0-9]+)")
 def vimeo_url(match):
     """vimeo <url> - returns information on the Vimeo video at <url>"""
-    data = http.get_json('http://vimeo.com/api/v2/video/%s.json'
-                         % match.group(1))
+    video_id = match.group(1)
+    data = http.get_json("http://vimeo.com/api/v2/video/{}.json".format(video_id))
 
     if not data:
         return
@@ -14,12 +14,10 @@ def vimeo_url(match):
     info = data[0]
 
     info["duration"] = timeformat.format_time(info["duration"])
-    info["stats_number_of_likes"] = format(
-        info["stats_number_of_likes"], ",d")
-    info["stats_number_of_plays"] = format(
-        info["stats_number_of_plays"], ",d")
-    return ("\x02%(title)s\x02 - length \x02%(duration)s\x02 - "
-            "\x02%(stats_number_of_likes)s\x02 likes - "
-            "\x02%(stats_number_of_plays)s\x02 plays - "
-            "\x02%(user_name)s\x02 on \x02%(upload_date)s\x02"
-            % info)
+
+    return (
+        "\x02{title}\x02 - length \x02{duration}\x02 - "
+        "\x02{stats_number_of_likes:,d}\x02 likes - "
+        "\x02{stats_number_of_plays:,d}\x02 plays - "
+        "\x02{user_name}\x02 on \x02{upload_date}\x02".format_map(info)
+    )
