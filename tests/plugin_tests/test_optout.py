41a322c2bdcf6fc72d859c64ecf6580d7ca7e00a
linuxdaemon <linuxdaemon@snoonet.org>
2019-06-06 22:35:28 -0500

    Fix ambiguous variable name

--- tests/plugin_tests/test_optout.py
+++ tests/plugin_tests/test_optout.py
@@ -2,14 +2,14 @@ from plugins.core.optout import get_conn_optouts, optout_cache
 
 
 def test_conn_case():
-    l = optout_cache['TestConnection']
+    conn_list = optout_cache['TestConnection']
 
-    assert get_conn_optouts('TestConnection') is l
-    assert get_conn_optouts('testconnection') is l
-    assert get_conn_optouts('testconnection1') is not l
+    assert get_conn_optouts('TestConnection') is conn_list
+    assert get_conn_optouts('testconnection') is conn_list
+    assert get_conn_optouts('testconnection1') is not conn_list
 
-    l = optout_cache['testconnection']
+    conn_list = optout_cache['testconnection']
 
-    assert get_conn_optouts('TestConnection') is l
-    assert get_conn_optouts('testconnection') is l
-    assert get_conn_optouts('testconnection1') is not l
+    assert get_conn_optouts('TestConnection') is conn_list
+    assert get_conn_optouts('testconnection') is conn_list
+    assert get_conn_optouts('testconnection1') is not conn_list
