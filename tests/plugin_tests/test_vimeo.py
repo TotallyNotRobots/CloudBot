0429cb7492075a81f97bdcb371994643b634dd24
linuxdaemon <linuxdaemon@snoonet.org>
2019-06-15 13:20:29 -0500

    tests: Add basic tests for vimeo.py

---
+++ tests/plugin_tests/test_vimeo.py
@@ -0,0 +1,100 @@
+import re
+
+import pytest
+from mock import patch
+
+
+@pytest.fixture()
+def patch_get_json():
+    with patch('cloudbot.util.http.get_json') as patched:
+        yield patched
+
+
+regexp = re.compile(r"vimeo.com/([0-9]+)")
+
+
+def test_no_data(patch_get_json):
+    from plugins import vimeo
+
+    patch_get_json.return_value = []
+
+    result = vimeo.vimeo_url(regexp.search("https://vimeo.com/1125483"))
+
+    assert result is None
+
+
+def test_no_likes(patch_get_json):
+    from plugins import vimeo
+
+    patch_get_json.return_value = [
+        {
+            "id": 11235,
+            "title": "A video title",
+            "description": "Some video description",
+            "url": "https://vimeo.com/112345",
+            "upload_date": "2006-10-24 22:56:45",
+            "user_id": 554,
+            "user_name": "AUser",
+            "user_url": "https://vimeo.com/user554",
+            "stats_number_of_plays": 106,
+            "stats_number_of_comments": 6,
+            "duration": 44,
+            "width": 320,
+            "height": 240,
+            "tags": "foo, bar",
+            "embed_privacy": "anywhere"
+        }
+    ]
+
+    result = vimeo.vimeo_url(regexp.search("https://vimeo.com/11235"))
+
+    expected_parts = [
+        '\x02A video title\x02',
+        'length \x0244 seconds\x02',
+        '\x020\x02 likes',
+        '\x02106\x02 plays',
+        '\x02AUser\x02 on \x022006-10-24 22:56:45\x02',
+    ]
+
+    expected = ' - '.join(expected_parts)
+
+    assert result == expected
+
+
+def test_with_likes(patch_get_json):
+    from plugins import vimeo
+
+    patch_get_json.return_value = [
+        {
+            "id": 11235,
+            "title": "A video title",
+            "description": "Some video description",
+            "url": "https://vimeo.com/112345",
+            "upload_date": "2006-10-24 22:56:45",
+            "user_id": 554,
+            "user_name": "AUser",
+            "user_url": "https://vimeo.com/user554",
+            "stats_number_of_likes": 54,
+            "stats_number_of_plays": 106,
+            "stats_number_of_comments": 6,
+            "duration": 44,
+            "width": 320,
+            "height": 240,
+            "tags": "foo, bar",
+            "embed_privacy": "anywhere"
+        }
+    ]
+
+    result = vimeo.vimeo_url(regexp.search("https://vimeo.com/11235"))
+
+    expected_parts = [
+        '\x02A video title\x02',
+        'length \x0244 seconds\x02',
+        '\x0254\x02 likes',
+        '\x02106\x02 plays',
+        '\x02AUser\x02 on \x022006-10-24 22:56:45\x02',
+    ]
+
+    expected = ' - '.join(expected_parts)
+
+    assert result == expected
