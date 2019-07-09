b5000358d34a2dcb317189586f0fdec060f16bb4
linuxdaemon <linuxdaemon@snoonet.org>
2019-07-08 21:39:02 -0500

    Add sprunge.us pastebin implementation

---
+++ plugins/pastebins/sprunge.py
@@ -0,0 +1,46 @@
+import requests
+from requests import HTTPError, RequestException
+
+from cloudbot import hook
+from cloudbot.util.web import Pastebin, pastebins, ServiceHTTPError, ServiceError
+
+
+class Sprunge(Pastebin):
+    def __init__(self, base_url):
+        super().__init__()
+        self.url = base_url
+
+    def paste(self, data, ext):
+        if isinstance(data, str):
+            encoded = data.encode()
+        else:
+            encoded = data
+
+        params = {
+            'params': encoded
+        }
+
+        try:
+            with requests.post(self.url, params=params) as response:
+                response.raise_for_status()
+                url = response.text.strip()
+        except HTTPError as e:
+            r = e.response
+            raise ServiceHTTPError(r.reason, r) from e
+        except RequestException as e:
+            raise ServiceError(e.request, "Connection error occurred") from e
+
+        if ext:
+            url += '?{}'.format(ext)
+
+        return url
+
+
+@hook.on_start()
+def register():
+    pastebins.register('sprunge', Sprunge('http://sprunge.us'))
+
+
+@hook.on_stop()
+def unregister():
+    pastebins.remove('sprunge')
