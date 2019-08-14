7ce071365911deb569c6e0270b33a105cc952d04
linuxdaemon <linuxdaemon@snoonet.org>
2019-08-14 11:16:37 -0500

    Expand metars tests

--- tests/plugin_tests/test_metars.py
+++ tests/plugin_tests/test_metars.py
@@ -29,7 +29,20 @@ def test_taf(mock_requests):
     from plugins.metars import taf
     assert taf('abcd') == 'ABCD: Foo Bar Test'
 
+
+def test_invalid_station(mock_requests):
+    from plugins.metars import taf
     invalid_station = "please specify a valid station code " \
                       "see http://weather.rap.ucar.edu/surface/stations.txt " \
                       "for a list."
     assert taf('abc') == invalid_station
+
+
+def test_station_not_found(mock_requests):
+    mock_requests.add(
+        mock_requests.GET, 'http://api.av-wx.com/taf/ABCD',
+        json={},
+        status=404,
+    )
+    from plugins.metars import taf
+    assert taf('abcd') == 'Station not found'
