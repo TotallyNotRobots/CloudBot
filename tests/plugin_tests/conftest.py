ebcc1a20c714351ca582645f5744240dfd119ec6
linuxdaemon <linuxdaemon@snoonet.org>
2019-08-16 10:14:04 -0500

    Add freeze_time test fixture

--- tests/plugin_tests/conftest.py
+++ tests/plugin_tests/conftest.py
@@ -1,3 +1,5 @@
+import datetime as _dt
+
 import pytest
 from mock import patch
 from responses import RequestsMock
@@ -39,3 +41,24 @@ def unset_bot():
     yield
     from cloudbot.bot import bot
     bot.set(None)
+
+
+class FrozenDatetime(_dt.datetime):
+    _now = None
+
+    @classmethod
+    def now(cls, *args, **kwargs):
+        if cls._now is None:
+            return super().now(*args, **kwargs)
+
+        return cls._now
+
+
+@pytest.fixture()
+def freeze_time():
+    import time
+    now = time.time()
+    dt_now = _dt.datetime.fromtimestamp(now)
+    with patch('time.time') as mocked, patch('datetime.datetime', new=FrozenDatetime):
+        _dt.datetime._now = dt_now
+        mocked.return_value = now
