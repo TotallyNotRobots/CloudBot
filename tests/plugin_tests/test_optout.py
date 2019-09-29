e1366ff74d4e5f1edd6a665a1a0a53eb893507dc
linuxdaemon <linuxdaemon@snoonet.org>
2019-09-29 06:50:12 -0500

    optout: Add tests for optout match

--- tests/plugin_tests/test_optout.py
+++ tests/plugin_tests/test_optout.py
@@ -1,3 +1,6 @@
+from mock import MagicMock, patch
+
+from plugins.core import optout
 from plugins.core.optout import get_conn_optouts, optout_cache
 
 
@@ -13,3 +16,43 @@ def test_conn_case():
     assert get_conn_optouts('TestConnection') is conn_list
     assert get_conn_optouts('testconnection') is conn_list
     assert get_conn_optouts('testconnection1') is not conn_list
+
+
+def test_ignore_core():
+    bot = MagicMock()
+    event = MagicMock()
+    _hook = MagicMock()
+
+    _hook.plugin.title = "core.optout"
+    _hook.function_name = "optout"
+
+    event.chan = "#test"
+    conn = event.conn
+
+    conn.name = "test"
+
+    opt = optout.OptOut("#test", "*", False)
+
+    with patch.dict(optout.optout_cache, clear=True, test=[opt]):
+        res = optout.optout_sieve(bot, event, _hook)
+        assert res is event
+
+
+def test_match():
+    bot = MagicMock()
+    event = MagicMock()
+    _hook = MagicMock()
+
+    _hook.plugin.title = "test"
+    _hook.function_name = "optout"
+
+    event.chan = "#test"
+    conn = event.conn
+
+    conn.name = "test"
+
+    opt = optout.OptOut("#test", "*", False)
+
+    with patch.dict(optout.optout_cache, clear=True, test=[opt]):
+        res = optout.optout_sieve(bot, event, _hook)
+        assert res is None
