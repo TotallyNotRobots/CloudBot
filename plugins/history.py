9f7cadfef74de0192eba337b431f6f72c1cec092
linuxdaemon <linuxdaemon@snoonet.org>
2019-09-29 11:30:43 -0500

    Split history and seen to separate plugins

--- plugins/history.py
+++ plugins/history.py
@@ -1,47 +1,8 @@
-import re
 import time
 from collections import deque
 
-from sqlalchemy import Table, Column, String, PrimaryKeyConstraint, Float, select, and_
-
 from cloudbot import hook
 from cloudbot.event import EventType
-from cloudbot.util import timeformat, database
-
-table = Table(
-    'seen_user',
-    database.metadata,
-    Column('name', String),
-    Column('time', Float),
-    Column('quote', String),
-    Column('chan', String),
-    Column('host', String),
-    PrimaryKeyConstraint('name', 'chan')
-)
-
-
-def track_seen(event, db):
-    """ Tracks messages for the .seen command
-    :type event: cloudbot.event.Event
-    :type db: sqlalchemy.orm.Session
-    """
-    # keep private messages private
-    now = time.time()
-    if event.chan[:1] == "#" and not re.findall('^s/.*/.*/$', event.content.lower()):
-        res = db.execute(
-            table.update().where(and_(
-                table.c.name == event.nick.lower(), table.c.chan == event.chan
-            )).values(time=now, quote=event.content, host=str(event.mask))
-        )
-        if res.rowcount == 0:
-            db.execute(
-                table.insert().values(
-                    name=event.nick.lower(), time=now, quote=event.content,
-                    chan=event.chan, host=str(event.mask)
-                )
-            )
-
-        db.commit()
 
 
 def track_history(event, message_time, conn):
@@ -62,9 +23,8 @@ def track_history(event, message_time, conn):
 
 
 @hook.event([EventType.message, EventType.action], singlethread=True)
-def chat_tracker(event, db, conn):
+def chat_tracker(event, conn):
     """
-    :type db: sqlalchemy.orm.Session
     :type event: cloudbot.event.Event
     :type conn: cloudbot.client.Client
     """
@@ -72,7 +32,6 @@ def chat_tracker(event, db, conn):
         event.content = "\x01ACTION {}\x01".format(event.content)
 
     message_time = time.time()
-    track_seen(event, db)
     track_history(event, message_time, conn)
 
 
@@ -89,36 +48,3 @@ async def resethistory(event, conn):
     except KeyError:
         # wat
         return "There is no history for this channel."
-
-
-@hook.command()
-def seen(text, nick, chan, db, event, is_nick_valid):
-    """<nick> <channel> - tells when a nickname was last in active in one of my channels
-
-    :type db: sqlalchemy.orm.Session
-    :type event: cloudbot.event.Event
-    """
-
-    if event.conn.nick.lower() == text.lower():
-        return "You need to get your eyes checked."
-
-    if text.lower() == nick.lower():
-        return "Have you looked in a mirror lately?"
-
-    if not is_nick_valid(text):
-        return "I can't look up that name, its impossible to use!"
-
-    last_seen = db.execute(
-        select([table.c.name, table.c.time, table.c.quote]).where(and_(
-            table.c.name == text.lower(), table.c.chan == chan
-        ))
-    ).fetchone()
-
-    if last_seen:
-        reltime = timeformat.time_since(last_seen[1])
-        if last_seen[2][0:1] == "\x01":
-            return '{} was last seen {} ago: * {} {}'.format(text, reltime, text, last_seen[2][8:-1])
-
-        return '{} was last seen {} ago saying: {}'.format(text, reltime, last_seen[2])
-
-    return "I've never seen {} talking in this channel.".format(text)
