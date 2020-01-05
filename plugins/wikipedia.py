40b15bcf79458ec632098e9b42c98cee4df88d96
znuxor <znuxor@gmail.com>
2020-01-05 16:25:20 -0500

    Added a new api request to fetch the description. The description is returned empty right now.

--- plugins/wikipedia.py
+++ plugins/wikipedia.py
@@ -11,6 +11,7 @@ from cloudbot.util.http import parse_xml
 
 api_prefix = "http://en.wikipedia.org/w/api.php"
 search_url = api_prefix + "?action=opensearch&format=xml"
+description_fetch_url = api_prefix + "?action=query&format=xml&prop=extracts&explaintext=true&exintro=true&exsectionformat=plain"
 random_url = api_prefix + "?action=query&format=xml&list=random&rnlimit=1&rnnamespace=0"
 
 paren_re = re.compile(r'\s*\(.*\)$')
@@ -40,12 +41,35 @@ def wiki(text, reply):
 
     def extract(item):
         return [item.find(ns + i).text for i in
-                ('Text', 'Description', 'Url')]
+                ('Text', 'Url')]
 
-    title, desc, url = extract(items[0])
+    title, url = extract(items[0])
+
+    def get_description(request_title):
+        try:
+            maximum_desc_len = 200
+            desc_request_params = {'exchars': maximum_desc_len,
+                                   'titles': request_title}
+            description_request = requests.get(description_fetch_url, params=desc_request_params)
+            description_request.raise_for_status()
+        except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as e:
+            reply("Could not get Wikipedia page description: {}".format(e))
+            raise
+
+        y = parse_xml(description_request.text)
+        try:
+            plain_desc = y.find('query').find('pages').find('page').find('extract').text
+        except AttributeError as e:
+            reply("Could not extract the description of the article: {}".format(e))
+            raise
+
+        return plain_desc
+
+    desc = get_description(title)
 
     if 'may refer to' in desc:
-        title, desc, url = extract(items[1])
+        title, url = extract(items[1])
+        desc = get_description(title)
 
     title = paren_re.sub('', title)
 
