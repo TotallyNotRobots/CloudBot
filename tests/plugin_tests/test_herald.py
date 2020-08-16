e3648068c6a55cb9efa0f4552f9769f86353f20e
Aspen Barnes <wbarnes@etsy.com>
2020-08-16 14:28:45 -0500

    Add improved handling of herald spam
    Fixes #20

--- tests/plugin_tests/test_herald.py
+++ tests/plugin_tests/test_herald.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from unittest.mock import MagicMock
 
 import pytest
@@ -13,40 +14,69 @@ def clear_cache():
         yield
     finally:
         herald.herald_cache.clear()
+        herald.user_join.clear()
 
 
-@pytest.fixture()
-def unset_timeout():
-    try:
-        yield
-    finally:
-        herald.floodcheck.clear()
-
-
-@pytest.mark.usefixtures("unset_timeout")
 class TestWelcome:
     def _run(self):
         conn = MagicMock()
         event = Event(
-            hook=MagicMock(),
-            bot=conn.bot,
-            conn=conn,
-            channel='#foo',
-            nick='foobaruser'
+            hook=MagicMock(), bot=conn.bot, conn=conn, channel="#foo", nick="foobaruser"
         )
         return wrap_hook_response(herald.welcome, event)
 
-    def test_no_herald(self):
+    def test_no_herald(self, clear_cache):
         result = self._run()
         assert result == []
 
-    @pytest.mark.parametrize('text,out', [
-        ('Hello world', '\u200b Hello world'),
-        ('bino', '\u200b flenny'),
-        ('\u200b hi', '\u200b \u200b hi'),
-        ('o\u200b<', 'DECOY DUCK --> o\u200b<'),
-    ])
+    @pytest.mark.parametrize(
+        "text,out",
+        [
+            ("Hello world", "\u200b Hello world"),
+            ("bino", "\u200b flenny"),
+            ("\u200b hi", "\u200b \u200b hi"),
+            ("o\u200b<", "DECOY DUCK --> o\u200b<"),
+        ],
+    )
     def test_char_strip(self, clear_cache, text, out):
-        herald.herald_cache['#foo']['foobaruser'] = text
+        herald.herald_cache["#foo"]["foobaruser"] = text
         result = self._run()
-        assert result == [('message', ('#foo', out))]
+        assert result == [("message", ("#foo", out))]
+
+    def test_flood(self, clear_cache, freeze_time):
+        chan = "#foo"
+        nick = "foobaruser"
+        nick1 = "foonick"
+        chan1 = "#barchan"
+        herald.herald_cache[chan].update({nick: "Some herald", nick1: "Other herald,"})
+        herald.herald_cache[chan1].update(
+            {nick: "Someother herald", nick1: "Yet another herald"}
+        )
+        conn = MagicMock(name="fooconn")
+        conn.mock_add_spec(["name", "bot", "action", "message", "notice"])
+        event = Event(conn=conn, bot=conn.bot, channel=chan, nick=nick)
+        event1 = Event(conn=conn, bot=conn.bot, channel=chan, nick=nick1)
+        event2 = Event(conn=conn, bot=conn.bot, channel=chan1, nick=nick,)
+        event3 = Event(conn=conn, bot=conn.bot, channel=chan1, nick=nick1)
+
+        def check(ev):
+            return wrap_hook_response(herald.welcome, ev)
+
+        assert check(event) == [("message", ("#foo", "\u200b Some herald"))]
+
+        assert check(event) == []
+        assert check(event1) == []
+
+        assert check(event2) == [("message", ("#barchan", "\u200b Someother herald"))]
+        assert check(event3) == []
+
+        freeze_time.tick(timedelta(seconds=10))
+        # Channel spam time expired
+        assert check(event1) == [("message", ("#foo", "\u200b Other herald,"))]
+        # User spam time still in effect
+        assert check(event) == []
+
+        freeze_time.tick(timedelta(minutes=5))
+
+        # User spam time expired
+        assert check(event) == [("message", ("#foo", "\u200b Some herald"))]
