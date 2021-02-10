cf472da48e22361144a301039dcb438a86b8d8ad
linuxdaemon <linuxdaemon.irc@gmail.com>
2021-02-10 01:10:57 -0500

    Fix test timezones

--- tests/conftest.py
+++ tests/conftest.py
@@ -55,11 +55,7 @@ def mock_requests():
 def freeze_time():
     # Make sure some randomness in the time doesn't break a test
     dt = datetime.datetime(2019, 8, 22, 18, 14, 36)
-    diff = datetime.datetime.now() - datetime.datetime.utcnow()
-    ts = round(diff.total_seconds() / (15 * 60)) * (15 * 60)
-    tz = datetime.timedelta(seconds=ts)
-
-    with freezegun.freeze_time(dt, tz) as ft:
+    with freezegun.freeze_time(dt, tz_offset=-5) as ft:
         yield ft
 
 
