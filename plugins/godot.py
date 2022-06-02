d943ef09068355c8e807bb6a802ae887299a160a
Matheus Fillipe <matheusfillipeag@gmail.com>
2022-06-02 01:58:24 +0000

    godot docs

--- plugins/godot.py
+++ plugins/godot.py
@@ -1,8 +1,12 @@
 """Maybe more godot relatd stuff in the future."""
 
-from cloudbot import hook
 import datetime
-from gazpacho import get, Soup
+import urllib.parse
+
+from gazpacho import Soup, get
+
+from cloudbot import hook
+
 
 @hook.command(autohelp=False)
 def jamdate(reply):
@@ -14,7 +18,7 @@ def jamdate(reply):
     text = elm.text
     from_date = elm.find("span")[0].text
     to_date = elm.find("span")[1].text
-    reply(f"{text} from {from_date} to {to_date}")
+    reply(f"Ich.io Godot {text} from {from_date} to {to_date}")
     from_date = datetime.datetime.strptime(from_date, "%Y-%m-%d %H:%M:%S")
     to_date = datetime.datetime.strptime(to_date, "%Y-%m-%d %H:%M:%S")
     start_time_left = (from_date - datetime.datetime.utcnow())
@@ -22,3 +26,34 @@ def jamdate(reply):
 
     reply(f"Jam starts in {start_time_left.days} days {start_time_left.seconds//3600} hours {(start_time_left.seconds//60)%60} minutes.")
     reply(f"Jam ends in {end_time_left.days} days {end_time_left.seconds//3600} hours {(end_time_left.seconds//60)%60} minutes.")
+
+
+@hook.command()
+def godocs(text, reply):
+    """<text> - Searches on godot documentation"""
+    # url encode
+    query = urllib.parse.quote(text)
+    url = f"https://docs.godotengine.org/_/api/v2/search/?q={query}&project=godot&version=stable&language=en"
+    data = get(url)
+
+    i = 0
+    used = set()
+    for item in data['results']:
+        if i == 4:
+            break
+        description = ""
+        for block in item['blocks']:
+            if block['type'] == 'section':
+                if block['content']:
+                    limit = 128
+                    description += block['content'][:limit]
+                    if len(description) > limit:
+                        description += "..."
+                break
+
+        if item['path'] in used:
+            continue
+
+        i += 1
+        used.add(item['path'])
+        reply(f"{item['title']}: {description} - {item['domain'] + item['path']}")
