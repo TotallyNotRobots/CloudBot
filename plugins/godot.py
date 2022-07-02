ff57f68e6614091e188c54830e8aba12a0e9562a
Matheus Fillipe <matheusfillipeag@gmail.com>
2022-07-02 00:59:09 +0000

    godot wild jam: manually compute date

--- plugins/godot.py
+++ plugins/godot.py
@@ -11,21 +11,32 @@ from cloudbot import hook
 @hook.command(autohelp=False)
 def jamdate(reply):
     """- Next godot jam date"""
-    url_soup = Soup(get("https://godotwildjam.com"))
-    url = url_soup.find("a", {"class": "elementor-button-link"})[0].attrs['href']
-    soup = Soup(get(url))
-    elm = soup.find("div", {"class": "date_data"})
-    text = elm.text
-    from_date = elm.find("span")[0].text
-    to_date = elm.find("span")[1].text
-    reply(f"Ich.io Godot {text} from {from_date} to {to_date}")
-    from_date = datetime.datetime.strptime(from_date, "%Y-%m-%d %H:%M:%S")
-    to_date = datetime.datetime.strptime(to_date, "%Y-%m-%d %H:%M:%S")
+    try:
+        url_soup = Soup(get("https://godotwildjam.com"))
+        url = url_soup.find("a", {"class": "elementor-button-link"})[0].attrs['href']
+        soup = Soup(get(url))
+        elm = soup.find("div", {"class": "date_data"})
+        text = elm.text
+        from_date = elm.find("span")[0].text
+        to_date = elm.find("span")[1].text
+        reply(f"Ich.io Godot {text} from {from_date} to {to_date}")
+        from_date = datetime.datetime.strptime(from_date, "%Y-%m-%d %H:%M:%S")
+        to_date = datetime.datetime.strptime(to_date, "%Y-%m-%d %H:%M:%S")
+    except Exception:
+        now = datetime.datetime.utcnow()
+        firstday = now.replace(day=1)
+        # This is the friday after the 1st weekend
+        friday = 12 - firstday.weekday()
+        from_date = firstday.replace(day=friday, hour=20, minute=0)
+        to_date = from_date + datetime.timedelta(days=7)
+
     start_time_left = (from_date - datetime.datetime.utcnow())
     end_time_left = (to_date - datetime.datetime.utcnow())
 
-    reply(f"Jam starts in {start_time_left.days} days {start_time_left.seconds//3600} hours {(start_time_left.seconds//60)%60} minutes.")
-    reply(f"Jam ends in {end_time_left.days} days {end_time_left.seconds//3600} hours {(end_time_left.seconds//60)%60} minutes.")
+    reply(
+        f"Jam starts in {start_time_left.days} days {start_time_left.seconds//3600} hours {(start_time_left.seconds//60)%60} minutes.")
+    reply(
+        f"Jam ends in {end_time_left.days} days {end_time_left.seconds//3600} hours {(end_time_left.seconds//60)%60} minutes.")
 
 
 @hook.command()
