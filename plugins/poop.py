94d5c0d10b404c7580f3e222d7463d58b3f8aa75
Matheus Fillipe <matheusfillipeag@gmail.com>
2022-08-18 05:52:57 +0000

    Added poop

---
+++ plugins/poop.py
@@ -0,0 +1,22 @@
+# Funny prank
+
+from random import choice
+
+from cloudbot import hook
+
+SENTENCES = [
+    "Leaves a dump on {}'s head",
+    "Poops hard over {}'s foot",
+    "Sharts on {}'s face",
+    "Defecates out of control all around {}",
+    "Farts and poops while {} smells it",
+    "Feeds {} by pooping on his mouth,"
+]
+
+
+@hook.command("poop", autohelp=False)
+def poop(text):
+    """<nick> -- Makes the bot poop"""
+    if not text.strip():
+        return "💩"
+    return choice(SENTENCES).format(text.strip())
