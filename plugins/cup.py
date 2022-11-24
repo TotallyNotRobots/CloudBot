4d02a8bc03e2800d234a730abd92e37d3c96bf53
Matheus Fillipe <matheusfillipeag@gmail.com>
2022-11-24 23:55:18 +0000

    update formatting

--- plugins/cup.py
+++ plugins/cup.py
@@ -6,17 +6,32 @@ import requests
 
 API = "http://api.cup2022.ir/api/v1/{}"
 
+
 def get(url, bearer):
     return requests.get(url, headers={"Authorization": "Bearer " + bearer, "Content-Type": "application/json"}).json()
 
+
+def get_token(email, password):
+    json_data = {
+        'email': email,
+        'password': password,
+    }
+
+    response = requests.post('http://api.cup2022.ir/api/v1/user/login', json=json_data)
+    return response.json()['data']['token']
+
+
 @hook.command("cup")
 def cup(text):
-    api = bot.config.get_api_key("cup_api")
+    email = bot.config.get_api_key("cup_api_email")
+    password = bot.config.get_api_key("cup_api_password")
+    api = get_token(email, password)
     matches = get(API.format("match"), api)['data']
     matches_result = []
     # sort matches by date
     dateformat = "%m/%d/%Y %H:%M"
-    matches = sorted(matches, key=lambda k: datetime.strptime(k['local_date'], dateformat))
+    matches = sorted(matches, key=lambda k: datetime.strptime(
+        k['local_date'], dateformat))
     for match in matches:
         # only get matches that are in 2 days range
         date = datetime.strptime(match["local_date"], dateformat)
@@ -29,7 +44,10 @@ def cup(text):
         if match["time_elapsed"] not in ["notstarted", "finished"]:
             prepend = "⚽️ "
             append = f'  time: {match["time_elapsed"]}'
+        if match["time_elapsed"] == "finished":
+            prepend = "🏁 "
 
-        matches_result.append(f'{prepend}{match["local_date"]}  {match["home_team_en"]} vs {match["away_team_en"]}    score: {match["home_score"]} - {match["away_score"]}{append}')
+        matches_result.append(
+            f'{prepend}{match["local_date"]}  {match["home_team_en"]} vs {match["away_team_en"]}    score: {match["home_score"]} - {match["away_score"]}{append}')
 
     return matches_result
