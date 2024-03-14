ad893dbe7193b33da391908e992e279b98f3c9dc
linuxdaemon <linuxdaemon.irc@gmail.com>
2024-03-14 15:19:32 +0000

    Fix type comments

--- tests/util/mock_irc_client.py
+++ tests/util/mock_irc_client.py
@@ -6,5 +6,5 @@ from cloudbot.clients.irc import IrcClient
 class MockIrcClient(IrcClient):
     def __init__(self, bot, name, nick, config):
         super().__init__(bot, "irc", name, nick, config=config)
-        self.connect = MagicMock()  # type: ignore[assignment]
-        self.send = MagicMock()  # type: ignore[assignment]
+        self.connect = MagicMock()  # type: ignore[method-assign]
+        self.send = MagicMock()  # type: ignore[method-assign]
