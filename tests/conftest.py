bd42e49872a04773309621f6b649cb925acaec71
linuxdaemon <linuxdaemon.irc@gmail.com>
2024-03-15 00:20:47 +0000

    Fix test errors in plugin manager

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import datetime
+import importlib
 import logging
 from typing import List
 from unittest.mock import MagicMock, patch
@@ -30,13 +31,13 @@ def caplog_bot(caplog):
 
 @pytest.fixture()
 def patch_import_module():
-    with patch("importlib.import_module") as mocked:
+    with patch.object(importlib, "import_module") as mocked:
         yield mocked
 
 
 @pytest.fixture()
 def patch_import_reload():
-    with patch("importlib.reload") as mocked:
+    with patch.object(importlib, "reload") as mocked:
         yield mocked
 
 
