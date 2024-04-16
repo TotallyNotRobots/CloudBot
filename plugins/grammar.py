7c839101431544b28d2dd37a362dcc37521689ec
Matheus Fillipe <matheusfillipeag@gmail.com>
2024-04-16 12:32:35 +0000

    enhanced grammar.

--- plugins/grammar.py
+++ plugins/grammar.py
@@ -1,10 +1,15 @@
 import json
+import re
+from time import sleep
 
 import requests
+
 from cloudbot import hook
 
-MODEL = "vennify/t5-base-grammar-correction"
-API_URL = f"https://api-inference.huggingface.co/models/{MODEL}"
+LANG_MODEL_MAP = {"en": "vennify/t5-base-grammar-correction",
+                  "de": "MRNH/mbart-german-grammar-corrector",
+                  }
+API_URL = "https://api-inference.huggingface.co/models/{model}"
 
 
 class HuggingFaceClient:
@@ -19,30 +24,65 @@ class HuggingFaceClient:
     def next_token(self) -> str:
         return next(self.api_tokens)
 
-    def _send(self, payload: dict) -> dict:
+    def _send(self, payload: dict, model: str) -> dict:
         data = json.dumps(payload)
-        response = self.session.request("POST", API_URL, data=data)
+        response = self.session.request("POST", API_URL.format(model=model), data=data)
         obj = json.loads(response.content.decode("utf-8"))
         return obj
 
-    def send(self, text: str) -> dict:
+    def send(self, text: str, model: str) -> dict:
         inputs = {"inputs": text}
-        return self._send(inputs)
+        return self._send(inputs, model)
 
 
-@hook.command("grammar")
-def grammar(text, bot, reply):
+def grammar(text, bot, reply, lang="en", retry=True):
     api_key = bot.config.get_api_key("huggingface")
     if not api_key:
         return "error: missing api key for huggingface"
 
-    client = HuggingFaceClient([api_key])
-    response = client.send(text)
-    if "error" in response:
-        return response["error"] + ". Maybe try again later?"
+    if lang not in LANG_MODEL_MAP:
+        return f"error: language '{lang}' not supported"
 
-    generated_text = [r["generated_text"].strip() for r in response]
+    model = LANG_MODEL_MAP[lang]
+
+    text = text.strip()
+    if not text.endswith("."):
+        text = text + "."
+
+    client = HuggingFaceClient([api_key])
+    response = client.send(text, model)
+    if 'estimated_time' in response and "error" in response and "currently loading" in response["error"] and retry:
+        estimated_time = int(response['estimated_time'])
+        if estimated_time < 60 and estimated_time > 0:
+            reply(
+                "⏳ Model is currently loading. I will retry in a few minutes and give your response. Please don't spam.")
+            sleep(estimated_time)
+            return grammar(text, bot, reply, lang, retry=False)
+        else:
+            reply("⏳ Model is currently loading and will take some minutes. Try again later. estimated time: " + str(
+                estimated_time))
+
+    if "error" in response:
+        return response["error"]
+
+    def proccess_response(resp: str) -> str:
+        resp = resp.strip()
+        # Replace " ." in endings with "."
+        resp = re.sub(r"\s+\.$", ".", resp)
+        return resp
+
+    generated_text = [proccess_response(r["generated_text"]) for r in response]
     if text.strip() in generated_text:
         return "✅ Perfect grammar! No changes needed."
 
-    return ". ".join(generated_text)
+    return " - ".join(generated_text)
+
+
+@hook.command("grammar", "grammaren")
+def grammar_command(text, message, bot, reply):
+    return grammar(text, bot, reply, lang="en")
+
+
+@hook.command("grammarde", "grammatik")
+def grammar_command2(text, message, bot, reply):
+    return grammar(text, bot, reply, lang="de")
