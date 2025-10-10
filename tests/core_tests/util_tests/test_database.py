728a72756399360f5f5d7cfde08f573162ad9b4c
linuxdaemon <linuxdaemon.irc@gmail.com>
2025-10-10 17:35:24 +0000

    refactor(db): remove global metadata binding
    This is removed in SQLAlchemy 2.0 and deprecated in the current version

--- tests/core_tests/util_tests/test_database.py
+++ tests/core_tests/util_tests/test_database.py
@@ -3,7 +3,7 @@ from cloudbot.util import database
 
 def test_database(mock_db):
     database.configure()
-    assert database.metadata.bind is None
+    assert database.Session().bind is None
     engine = mock_db.engine
     database.configure(engine)
-    assert database.metadata.bind is engine
+    assert database.Session().bind is engine
